f778fdc0842341210b159fbfafff5d276268388a
amaliartnaa <amaliaratna41@gmail.com>
2024-01-23 18:58:04 +0700

    fetch API

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -1,45 +1,81 @@
 <template>
-    <div class="product-page">
-        <div>
+    <div
+        class="product-page" 
+        :class="!isProductAvailable ? 'bg-gray' : products.data.category === 'men\'s clothing' ? 'bg-skyBlue' : 'bg-lightPurple'
+    ">
+        <div v-if="loading">
             <Loading />
         </div>
         <div class="product-card">
 
             <!-- Product is not available -->
-            <div class="product-not-available">
+            <div 
+                class="product-not-available"
+                :class="isProductAvailable ? 'display-none' : 'product-not-available'"
+            >
                 <p class="text-no">This product is unavailable to show</p>
-                <button class="btn-not-available">Next product</button>
+                <button class="btn-not-available" @click="singleProduct()">Next product</button>
             </div>
 
             <!-- Product is available -->
-            <div class="product-available">
+            <div 
+                class="product-available"
+                :class="!isProductAvailable ? 'display-none' : 'product-available'"
+            >
 
                 <!-- Left -->
                 <div class="product-card-left">
-                    <img src="" alt="product" class="product-img">
+                    <img :src="products?.data?.image" alt="product" class="product-img">
                 </div>
 
                 <!-- Right -->
                 <div class="product-card-right">
-                    <h1 class="product-title">Hello</h1>
+                    <h1 
+                        class="product-title"
+                        :class="products?.data?.category === 'men\'s clothing' ? 'color-navy' : 'color-darkPurple'"
+                    >
+                        {{ products?.data?.title }}
+                    </h1>
                     <div class="product-info">
-                        <p class="product-category">hello</p>
+                        <p class="product-category">{{ products?.data?.category }}</p>
                         <div class="product-rating">
-                            <p class="product-rating-num"> / 5</p>
+                            <p class="product-rating-rate">{{ products?.data?.rating?.rate }} / 5</p>
                             <div class="product-rating-circle">
-                                <div class="circle"></div>
+                                <div 
+                                    v-for="maxRate in maxRates"
+                                    :key="maxRate"
+                                    class="circle"
+                                    :class="products?.data?.category === 'men\'s clothing' ? 'color-navy' : 'color-darkPurple'"
+                                ></div>
                             </div>
                         </div>
                     </div>
                     <hr />
 
-                    <p class="product-desc"></p>
+                    <p class="product-desc">{{ products?.data?.description }}</p>
                     <div class="down">
-                        <hr />
-                        <h2 class="product-price"></h2>
+                        <hr />   
+                        <h2 
+                            class="product-price"
+                            :class="products?.data?.category === 'men\'s clothing' ? 'color-navy' : 'color-darkPurple'"
+                        >
+                            ${{ products?.data?.price }}
+                        </h2>
                         <div class="btn-container">
-                            <button class="btn">Buy Now</button>
-                            <button class="btn btn-outline">Next Product</button>
+                            <button 
+                                class="btn"
+                                :class="products?.data?.category === 'men\'s clothing' ? 'font-white bg-navy' : 'font-white bg-darkPurple'"
+                            >
+                                Buy Now
+                            </button>
+                            <button
+                                @click="singleProduct()"
+                                class="btn btn-outline"
+                                :class="products?.data?.category === 'men\'s clothing' ? 'border-navy' : 'border-darkPurple'"
+                                
+                            >
+                                Next Product
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -52,7 +88,7 @@
 import Loading from '../components/Loading.vue'
 
 export default {
-    name: 'ProductDisplayComponent',
+    name: 'ProductPageComponent',
     components: {
         Loading,
     },
@@ -92,7 +128,7 @@ export default {
 
             const data = await this.API()
             if (data.category === "men's clothing" || data.category === "women's clothing") {
-                this.product = { data }
+                this.products = { data }
                 this.isProductAvailable = true
             } else {
                 this.isProductAvailable = false
